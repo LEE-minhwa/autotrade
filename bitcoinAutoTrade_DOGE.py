3651058ff7ee9e92ea15bf78a886c4d5764f4d2c
LEE-minhwa <83169906+LEE-minhwa@users.noreply.github.com>
2021-04-25 22:38:14 +0900

    Add files via upload

--- bitcoinAutoTrade_DOGE.py
+++ bitcoinAutoTrade_DOGE.py
@@ -5,7 +5,7 @@ import requests
 
 access = "5EFyZxldBNoX3sdrm9JCGTxo8goaJNd9CezhmgwP"
 secret = "W4yijzxKU9YeLoBZn6nhc7iGfU6jnsObUPoIlU3Q"
-myToken = "xoxb-1998829143459-2022495266384-wrZB4gpuCfxCiH6xscqaIEe1"
+myToken = "xoxb-1998829143459-2022495266384-nZknkOAN3yfN8HbJWIRKAiSr"
 
 def post_message(token, channel, text):
     """슬랙 메시지 전송"""
@@ -52,19 +52,21 @@ while True:
         now = datetime.datetime.now()
         start_time = get_start_time("KRW-DOGE") #9:00
         end_time = start_time + datetime.timedelta(days=1) #9:00 + 1일
-
+        # post_message(myToken,"#crypto", "running")
         # 9:00 < 현재 < #8:59:50
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-DOGE", 0.2)  # k값 변화에 따라
+            target_price = get_target_price("KRW-DOGE", 0.002)  # k값 변화에 따라
             current_price = get_current_price("KRW-DOGE")
             if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-DOGE", krw*0.9995)
+                    buy_result = upbit.buy_market_order("KRW-DOGE", krw*0.9995)
+                    post_message(myToken,"#crypto", "DOGE buy : " +str(buy_result))
         else:
             btc = get_balance("DOGE")
             if btc > 0.00008:
-                upbit.sell_market_order("KRW-DOGE", btc*0.9995)
+                sell_result = upbit.sell_market_order("KRW-DOGE", btc*0.9995)
+                post_message(myToken,"#crypto", "DOGE buy : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
