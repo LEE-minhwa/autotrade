5bd7ffb9d87918d97abac797dfea8d9954b14f40
LEE-minhwa <83169906+LEE-minhwa@users.noreply.github.com>
2021-04-27 23:32:51 +0900

    Add files via upload

--- bitcoinAutoTrade_DOGE copy.py
+++ bitcoinAutoTrade_DOGE copy.py
@@ -7,7 +7,7 @@ import threading
 
 access = "5EFyZxldBNoX3sdrm9JCGTxo8goaJNd9CezhmgwP"
 secret = "W4yijzxKU9YeLoBZn6nhc7iGfU6jnsObUPoIlU3Q"
-myToken = "xoxb-1998829143459-2022495266384-8AluetTyDwS17Sb3WStsNf1h"
+myToken = "xoxb-1998829143459-2022495266384-TgGx6IDR7tfgxDFYqoZx01eY"
 
 def fun_timer():
     timer = threading.Timer(600,fun_timer)
